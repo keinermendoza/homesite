a330166b255ec97031c4efb69b00877e4ff7bf17
keiner <keinermendoza@gmail.com>
2023-10-22 12:48:49 -0400

    finishing the layout for projects section

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,8 @@ def home(request):
             "url": "#projects",
         },
         {
-            "label": "Courses",
-            "url": "#courses",
+            "label": "Certificates",
+            "url": "#certificates",
         },
         {
             "label": "FAQ",
@@ -24,4 +24,51 @@ def home(request):
         },
     ]
 
-    return render(request, "blog/homepage.html", {"links": links})
+    projects = [
+    {
+        'image' :'https://m.media-amazon.com/images/I/71k06Iic2NL.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+        'name' : 'RedThunder K84 Wireless',
+        'description' : 'High-performance 2.4G Wireless Keyboard and Mouse】Are you looking for a small wireless keyboard and mouse...',
+        'tags' : [
+            'tecnology', 'keyboard', 'computers',
+        ],
+
+        'code':'urldecode',
+        'demo':'urldedemo',
+        'online':'urlonline',
+        'post':'urlpost',
+    },
+    {
+        'image' :'https://m.media-amazon.com/images/I/61PnHlc0HCL.__AC_SY445_SX342_QL70_FMwebp_.jpg',
+        'name' : 'Apple iPad',
+        'description' : 'WHY IPAD — All the essentials in the most affordable iPad, with a beautiful 10.2-inch Retina display, powerful...',
+        'tags' : [
+            'tecnology', 'pad', 'tablet',
+        ],
+        'demo':'urldedemo',
+        'certified':'urlcertified',
+        'online':'urlonline',
+    },
+    {
+        'image' :'https://m.media-amazon.com/images/I/61PnHlc0HCL.__AC_SY445_SX342_QL70_FMwebp_.jpg',
+        'name' : 'Apple iPad',
+        'description' : 'WHY IPAD — All the essentials in the most affordable iPad, with a beautiful 10.2-inch Retina display, powerful...',
+        'tags' : [
+            'tecnology', 'pad', 'tablet',
+        ],
+        'code':'urldecode',
+        'demo':'urldedemo',
+    },
+    {
+        'image' :'https://m.media-amazon.com/images/I/71k06Iic2NL.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+        'name' : 'RedThunder K84 Wireless',
+        'description' : 'High-performance 2.4G Wireless Keyboard and Mouse】Are you looking for a small wireless keyboard and mouse...',
+        'tags' : [
+            'tecnology', 'keyboard', 'computers',
+        ],
+        'post':'urlpost',
+        'code':'urldecode',
+    },
+]
+    return render(request, "blog/homepage.html", {"links": links,
+                                                  'projects':projects})
